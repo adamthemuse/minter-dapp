4cfec17037b1747216810d8e68cd7787871c80be
adamthemuse <adam@calyonmetalab.io>
2022-08-25 22:30:55 -0700

    updated slide images

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; // Replace with your own contract address
+const contractAddress = "0x26E67bd2b4af4eDE55129E4F54B30C72d1Ff433A"; // Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby, polygon, or ethereum
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
